9c43a1412931f26fe3f13b97529f754e7c66852c
Simon Meggle <simon.meggle@consol.de>
2012-07-13 14:14:14 +0200

    added SAHI_EXT_CLASS_PATH

--- userdata/bin/start_sahi.bat
+++ userdata/bin/start_sahi.bat
@@ -1,5 +1,5 @@
 @ECHO OFF
 SET SAHI_HOME=..\..
 SET SAHI_USERDATA_DIR=..
-SET SAHI_EXT_CLASS_PATH=
+SET SAHI_EXT_CLASS_PATH=%EXT_CLASS_PATH%;..\extlib\db\mysql-connector-java-5.1.21-bin.jar
 CALL %SAHI_HOME%\bin\sahi.bat
