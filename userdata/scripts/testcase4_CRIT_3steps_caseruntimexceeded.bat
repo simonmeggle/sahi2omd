b2ca8f36c638fda0e03d33afd8d422233f7ca012
Simon Meggle <simon.meggle@consol.de>
2012-07-16 16:07:55 +0200

    Added 4th testcase.

---
+++ userdata/scripts/testcase4_CRIT_3steps_caseruntimexceeded.bat
@@ -0,0 +1,2 @@
+@echo off
+C:\Windows\System32\cscript.exe //Nologo C:\sahi\userdata\bin\sahi2omd.vbs -mode nsca -m 1 -f testcases\testcase4_CRIT_3steps_caseruntimexceeded.sah -b firefox -u http://oxid/shop/ -n omd1 -h sahidose -s testcase
