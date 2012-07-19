8cb0d85302026a3f4f406bfad01238c6c8c40b32
Simon Meggle <simon.meggle@consol.de>
2012-07-19 18:47:16 +0200

    BULK COMMIT: DB save completed.

---
+++ userdata/scripts/Kopie von DB_testcase5_CRIT_3_3steps_except.bat
@@ -0,0 +1,2 @@
+@echo off
+C:\Windows\System32\cscript.exe //Nologo C:\sahi\userdata\bin\sahi2omd.vbs -mode db -m 1 -f testcases\foo.sah -b firefox -u http://oxid/shop/ -n omd1 -h sahidose -s testcase
