b8d2fbc7539edf25063f61eeb1242b019c48efd3
Simon Meggle <simon.meggle@consol.de>
2012-10-11 18:50:42 +0200

    Added testcases for NSCA - PhantomJS.

---
+++ userdata/scripts/nsca_PJS_testcase2_WARN_2_3steps_caseruntimexceeded.bat
@@ -0,0 +1,2 @@
+@echo off
+C:\Windows\System32\cscript.exe //Nologo C:\sahi\userdata\bin\sahi2omd.vbs -mode nsca -m 100 -f testcases\nsca_PJS_2_W_3stp_crtexc.sah -b phantomjs -u http://oxid/shop/ -n omd1 -h sahidose -s nsca_PJS_2_W_3stp_crtexc.sah
---
+++ userdata/scripts/nsca_PJS_testcase5_random.bat
@@ -0,0 +1,2 @@
+@echo off
+C:\Windows\System32\cscript.exe //Nologo C:\sahi\userdata\bin\sahi2omd.vbs -mode nsca -m 100 -f testcases\nsca_PJS_5_random.sah -b phantomjs -u http://oxid/shop/ -n omd1 -h sahidose -s nsca_PJS_5_random.sah
