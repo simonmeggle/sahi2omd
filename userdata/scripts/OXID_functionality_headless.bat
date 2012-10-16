e7dc8cbe24e3fbb947a25b707437cc7aae29fd27
Simon Meggle <simon.meggle@consol.de>
2012-10-16 18:56:09 +0200

    Added OXID demo cases.

---
+++ userdata/scripts/OXID_functionality_headless.bat
@@ -0,0 +1,2 @@
+@echo off
+C:\Windows\System32\cscript.exe //Nologo C:\sahi\userdata\bin\sahi2omd.vbs -mode db -m 2 -f OXID_functionality.suite -b phantomjs -u http://oxid/shop/ 
---
+++ userdata/scripts/lieferadresse_aendern.bat
@@ -0,0 +1,2 @@
+@echo off
+C:\Windows\System32\cscript.exe //Nologo C:\sahi\userdata\bin\sahi2omd.vbs -mode nsca -m 1 -capture -f testcases\OXID_Lieferadresse.sah -b firefox -u http://oxid/shop/ -n omd1 -h sahidose -s OXID_Lieferadresse.sah
